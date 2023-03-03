16ae3ba9f46191688026e63af34ed075ae172cdb
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-03 10:00:36 +0100

    feat(components/Footer.jsx) - add css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
-import Logo from '../assets/LOGO.svg';
 import './Banner.css'
+import Logo from '../assets/LOGO.svg';
 
 function Banner() {
     return <img src={Logo} alt="Logo Kasa" className='kasa-logo'/>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import LogoFooter from '../assets/logo-footer.svg'
 function Footer() {
     return (
         <div className='footer'>
-            <img src={LogoFooter} alt="Kasa" />
+            <img src={LogoFooter} alt="Kasa" className='footer-logo'/>
             <p>© 2020 Kasa. All rights reserved</p>
         </div>
     )

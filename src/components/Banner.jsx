77a4c45100d7d8976f34d1d9254da27d6929089b
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-03 09:40:37 +0100

    feat(components/Banner.jsx) - change balise

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/LOGO.svg';
 import './Banner.css'
 
 function Banner() {
-    return <div className='kasa-logo'><img src={Logo} alt="Logo Kasa" /></div>
+    return <img src={Logo} alt="Logo Kasa" className='kasa-logo'/>
 }
 
 export default Banner

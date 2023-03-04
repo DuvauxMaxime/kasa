3cac1c533eea03f1b70f42c71f96f139112e5241
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-04 15:19:09 +0100

    feat(components/Navigation.jsx) - edit navLink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,12 +4,14 @@ import styles from './Navigation.module.css'
 export const Navigation = () => {
    return (
       <nav className={styles.navBar}>
-         <NavLink to="/" className={styles.navLink}>
-            Accueil
-         </NavLink>
-         <NavLink to="/a-propos/" className={styles.navLink}>
-            A Propos
-         </NavLink>
+         <ul>
+            <NavLink to="/" className={styles.navLink}>
+               <li>Accueil</li>
+            </NavLink>
+            <NavLink to="/a-propos/" className={styles.navLink}>
+               <li>A Propos</li>
+            </NavLink>
+         </ul>
       </nav>
    )
 }

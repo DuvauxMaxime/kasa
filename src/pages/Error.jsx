9e27d4a454b0fbbe03759a4812ab941977cd134a
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-13 09:32:38 +0100

    feat(pages/Error.jsx) - css page error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+import LogoHeader from '../components/LogoHeader';
 import styles from './Error.module.css';
 
 const Error = () => {
    return (
-      <main className={styles.container}>
-         <h1 className={styles.title}>404</h1>
-         <p className={styles.text}>
-            Oups! La page que vous demandez n'existe pas.
-         </p>
-         <a href="/" className={styles.link}>
-            Retourner sur la page d'accueil
-         </a>
-      </main>
+      <>
+         <header className={styles.header}>
+            <LogoHeader />
+            <Navigation />
+         </header>
+         <main className={styles.container}>
+            <h1 className={styles.title}>404</h1>
+            <p className={styles.text}>
+               Oups! La page que vous demandez n'existe pas.
+            </p>
+            <a href="/" className={styles.link}>
+               Retourner sur la page d'accueil
+            </a>
+         </main>
+         <Footer />
+      </>
    );
 };
 

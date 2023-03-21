c8fc24779b458845536cc942e867e47dfcf7ff1e
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-21 10:29:26 +0100

    feat(pages/Location.jsx) - component Carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,31 +1,41 @@
 import moveLeft from '../assets/moveLeft.svg';
 import moveRight from '../assets/moveRight.svg';
+import { useState } from 'react';
 import styles from './Carrousel.module.css';
 
 const Carrousel = ({ src, alt, data }) => {
+   const [index, setIndex] = useState(0);
    const tabPictures = data;
-   let defaultIndex = 0;
-   let defaultPicture = data[defaultIndex];
+   // let viewablePictureIndex = 0;
+   let viewablePicture = data[index];
    const numberOfPictures = tabPictures.length;
-   console.log('------------> Number Pictures <---------------');
-   console.log(numberOfPictures);
-   console.log('------------> Index <---------------');
-   console.log(tabPictures.indexOf(defaultPicture));
+   const leftMove = (e) => {
+      index > 0 && setIndex(index - 1);
+   };
+   const rightMove = (e) => {
+      index < numberOfPictures - 1 && setIndex(index + 1);
+   };
    return (
       <div className={styles.container}>
-         <img src={defaultPicture} alt="appartement" className={styles.image} />
+         <img
+            src={viewablePicture}
+            alt="appartement"
+            className={styles.image}
+         />
          <img
             src={moveLeft}
             className={styles.moveLeft}
             alt="flèche directionnelle gauche"
+            onClick={leftMove}
          />
          <img
             src={moveRight}
             className={styles.moveRight}
             alt="flèche directionnelle droite"
+            onClick={rightMove}
          />
          <p className={styles.indicator}>
-            {defaultIndex + 1}/{numberOfPictures}
+            {index + 1}/{numberOfPictures}
          </p>
       </div>
    );

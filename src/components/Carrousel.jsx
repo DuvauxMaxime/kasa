a7b4f6585ea7e1657017a61226bf0dec623ed572
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-28 09:19:17 +0200

    fix(components/Carrousel.jsx) - rewriting function prev & next

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -15,13 +15,11 @@ const Carrousel = ({ data }) => {
    let viewablePicture = data[index]; // Photo visible
    // Fonction image précédente
    const handlePrev = () => {
-      (index > 0 && setIndex(index - 1)) ||
-         (index === 0 && setIndex(tabPictures.length - 1));
+      index === 0 ? setIndex(tabPictures.length - 1) : setIndex(index - 1);
    };
    // Fonction image suivante
    const handleNext = () => {
-      (index < tabPictures.length - 1 && setIndex(index + 1)) ||
-         (index === tabPictures.length - 1 && setIndex(0));
+      index === tabPictures.length - 1 ? setIndex(0) : setIndex(index + 1);
    };
    // Fonction définir une image depuis un bullet point
    const moveBullet = (index) => {

bcc76da51bb336ec98eff713a545ab72942ba596
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-29 11:16:41 +0200

    feat(components/LogoHeader.jsx, Footer.jsx) - add logo link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import styles from './Footer.module.css';
 import logoFooter from '../assets/logoFooter.svg';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
    return (
       <footer className={styles.footer}>
-         <img src={logoFooter} alt="Kasa" className={styles.kasaLogo} />
+         <Link to="/" className={styles.kasaLogoLink}>
+            <img src={logoFooter} alt="Kasa" className={styles.kasaLogo} />
+         </Link>
          <p className={styles.text}>© 2020 Kasa. All rights reserved</p>
       </footer>
    );
